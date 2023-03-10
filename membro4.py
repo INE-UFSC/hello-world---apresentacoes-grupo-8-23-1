77b11751bcaf7f8cef40746ee5970b829f434b09
GustavoBodi <gugagustavo2012@gmail.com>
2023-03-10 09:34:25 -0300

    Update membro4.py

--- membro4.py
+++ membro4.py
@@ -1,2 +1,2 @@
-def gustavo():
+def hello():
   return "Gustavo Bodi, minhas linguagens favoritas são Rust e C++. Meus hobbies são ler e praticar calistenia. Espero nesta disciplina aprender os design patterns que desconheço."
